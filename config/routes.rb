ee0218976799eb34f490a8070b19a69b0e7b81f0
Ryan Erricson <ryanerricson@gmail.com>
2021-06-15 18:12:03 -0400

    Nested favorites under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :favorites
+  
   resources :breweries
-  resources :users
+  resources :users do
+    resources :favorites
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
