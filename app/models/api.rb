5dba32845f3f1dc253c0f721b0f26c1fac3a8f85
Ryan Erricson <ryanerricson@gmail.com>
2021-07-08 11:32:24 -0400

    Fixed api pull:

--- app/models/api.rb
+++ app/models/api.rb
@@ -17,12 +17,13 @@ class Api < ApplicationRecord
                  :uid => brewery_hash["id"],
                  :url => brewery_hash["website_url"]
             )
+           
             if b.longitude == nil || b.latitude == nil 
                 result = Geocoder.search("#{brewery_hash["address"]} #{brewery_hash["city"]}, #{brewery_hash["state"]}")
-                if result
-                coord = result.first.coordinates
-                b.latitude = coord[0]
-                b.longitude = coord[1]
+                if result && result.first
+                    coord = result.first.coordinates
+                    b.latitude = coord[0]
+                    b.longitude = coord[1]
                 end
             end
 
